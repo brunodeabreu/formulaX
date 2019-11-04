5092cc6510e6fdc63aed0aa31e661b9380eaa960
BrunoMachado <brunodeabreu@gmail.com>
2019-11-04 17:02:02 -0300

    m

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-0hbvSHpKAeVB5bcXjUT_woCFMmlZBSR'
+export_file_name = 'model_v1'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
